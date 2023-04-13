43f2be2f04b171c31857c67f0f4be2d72221578d
shell <zichenchen@qianxin.com>
2023-04-13 14:29:22 +0800

    update

--- Lib/sre_parse.py
+++ Lib/sre_parse.py
@@ -225,7 +225,7 @@ class Tokenizer:
         self.istext = isinstance(string, str)
         self.string = string
         if not self.istext:
-            string = str(string, 'utf-8')
+            string = str(string, 'latin1')
         self.decoded_string = string
         self.index = 0
         self.next = None
